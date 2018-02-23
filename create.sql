0cc6105d232564159081aecdb8452c999b48d790
kjnelsen <36718457+kjnelsen@users.noreply.github.com>
2018-02-23 16:22:05 -0600

    sql file

---
+++ create.sql
@@ -0,0 +1,16 @@
+DROP TABLE IF EXISTS Items;
+DROP TABLE IF EXISTS Bidders;
+DROP TABLE IF EXISTS Bids;
+DROP TABLE IF EXISTS Sellers;
+CREATE TABLE Items (
+  ItemID int,
+
+
+);
+CREATE TABLE Bidders;
+CREATE TABLE Bids;
+CREATE TABLE Sellers;
+
+
+.separator |
+.import bidders.dat
